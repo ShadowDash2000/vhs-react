b4ced675f98adb11795d980173aa545bb62756a7
Vladislav <vladislavtihonov790@gmail.com>
2025-06-07 01:20:48 +0300

    Крайне важное замечание, все провайдеры перенесены из Layout.tsx в main.tsx, где будет гарантированно, что провайдеры не будут ререндриться на каждый обновление стейта

---
+++ src/main.tsx
@@ -0,0 +1,36 @@
+import {createRoot} from 'react-dom/client';
+import {RouterProvider} from 'react-router-dom';
+import {router} from "./router/router";
+import './main.css';
+import {ChakraProvider, createSystem, defaultConfig, defineConfig} from "@chakra-ui/react";
+import {ColorModeProvider} from "./components/ui/color-mode.jsx";
+import {AppContextProvider} from "./context/AppContextProvider.js";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const colorModeConfig = {
+    forcedTheme: 'dark',
+};
+const themeConfig = defineConfig({
+    theme: {
+        semanticTokens: {
+            colors: {
+                bg: {
+                    panel: {value: '#303e54'},
+                },
+            },
+        },
+    },
+})
+const system = createSystem(defaultConfig, themeConfig);
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')!).render(
+    <ChakraProvider value={system}>
+        <ColorModeProvider {...colorModeConfig} />
+        <AppContextProvider>
+            <QueryClientProvider client={queryClient}>
+                <RouterProvider router={router}/>
+            </QueryClientProvider>
+        </AppContextProvider>
+    </ChakraProvider>
+)
