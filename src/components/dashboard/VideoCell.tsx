605d74676dde59c0c009ca02e6b1722908bdc270
Vladislav <vladislavtihonov790@gmail.com>
2025-06-11 20:57:49 +0300

    Правка Теперь для клика по видео доступен весь таргет Table.row

--- src/components/dashboard/VideoCell.tsx
+++ src/components/dashboard/VideoCell.tsx
@@ -1,6 +1,5 @@
 import {Table, Image} from "@chakra-ui/react";
 import {useAppContext} from "@context/AppContextProvider/AppContextProvider";
-import {NavLink} from "react-router-dom";
 import type {FC} from "react";
 import type {VideoRecord} from "@shared/types/types"
 
@@ -10,19 +9,13 @@ interface VideoCellProps {
 
 export const VideoCell: FC<VideoCellProps> = ({video}) => {
     const {pb} = useAppContext();
-    const link = `/dashboard/video/${video.id}`;
-
     return (
         <>
             <Table.Cell width="20%">
-                <NavLink to={link}>
-                    <Image width="100%" src={pb.files.getURL(video, video.preview)}/>
-                </NavLink>
+                <Image width="100%" src={pb.files.getURL(video, video.preview)}/>
             </Table.Cell>
             <Table.Cell>
-                <NavLink to={link}>
-                    {video.name}
-                </NavLink>
+                {video.name}
             </Table.Cell>
         </>
     )
