587f85df6f4f0a853c44954320b0c96228985d59
Vladislav <vladislavtihonov790@gmail.com>
2025-06-19 00:10:47 +0300

    исправлен NaN в useVideoUpload, коротковая запись в createCollection. Удалены storybook файлы

--- src/shared/helpers/createCollection.ts
+++ src/shared/helpers/createCollection.ts
@@ -8,12 +8,9 @@ type CollectionType = {
 }
 
 export const createCollection = <T extends CollectionType>(listResult: ListResult<T>) => {
-    const items: Array<SearchCollectionType> = [];
-    for (const item of listResult.items) {
-        items.push({
-            label: item.name,
-            value: item.id,
-        });
-    }
+    const items: Array<SearchCollectionType> = listResult.items.map(item => ({
+        label: item.name,
+        value: item.id
+    }));
     return createListCollection<SearchCollectionType>({items});
 }
